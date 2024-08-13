d8400efac0d7b2c45115cbb313a5924b7f98ac79
Alex <guardamar81@gmail.com>
2024-08-13 20:49:13 +0400

    main.py
    main.py

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import random
 
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram.types import Message, FSInputFile
 
+from gtts import gTTS
 
 from dotenv import load_dotenv
 import os
